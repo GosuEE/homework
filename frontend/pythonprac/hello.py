2822b96efdb4f8cd1389875f52e56e8538e83484
GosuEE <leemin727@naver.com>
2022-11-01 03:21:24 +0900

    practice_2

--- frontend/pythonprac/hello.py
+++ frontend/pythonprac/hello.py
@@ -1,1 +1,10 @@
-print('hello sparta!!')
+import requests # requests 라이브러리 설치 필요
+
+r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
+rjson = r.json()
+
+tempJson = rjson['RealtimeCityAir']['row']
+print(tempJson)
+
+for json in tempJson:
+    print(json['IDEX_MVL'])
